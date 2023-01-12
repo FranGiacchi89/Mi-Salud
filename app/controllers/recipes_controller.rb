4b8458b69f9204e2622e2c1abb1cbe846e12bffc
Francisco Giacchi <franciscogiacchi@outlook.com>
2023-01-12 16:03:52 -0300

    Controladores y vistas de recipes y habits

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,34 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = current_user.recipes
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      redirect_to recipe_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @recipe.destroy
+    redirect_to recipe_path, status: :see_other
+  end
+
+  private
+
+  def recipe_params
+    params.require(:report).permit(:date, :doctor_name)
   end
 end
