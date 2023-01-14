bf793f0a7d4164edc93fe00a0799ba6b9a183eba
Francisco Giacchi <franciscogiacchi@outlook.com>
2023-01-14 18:50:01 -0300

    Cambios en el new de recipes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-  layout "layout_aplication", only: [:home]
+  # layout "layout_aplication", only: [:home]
 
   def index
   end
