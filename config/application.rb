00d238b6d820ed7b4dfdc0743926c97bd36230ed
gaston henrich <henrichgaston@gmail.com>
2023-01-20 18:19:39 -0300

    cambio en habits

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module MiSalud
   class Application < Rails::Application
     config.generators do |generate|
@@ -15,7 +14,6 @@ module MiSalud
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
